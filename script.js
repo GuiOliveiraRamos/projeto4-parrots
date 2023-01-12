c6de249778109f0196a878f9e04b07beba6c6a67
Guilherme Oliveira <guioliveiraramos30@gmail.com>
2023-01-12 15:42:09 -0300

    implementação java

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-let message = prompt("Escolha o número de cartas para jogar");
-let numeroCartas = Number(message);
+let numeroCartas = Number("");
 
-if (numeroCartas % 2 == 0 && numeroCartas >= 4 && numeroCartas <= 14) {
-    alert("Você pode jogar");
+function verificadorQtdeCartas() {
+    if (numeroCartas % 2 !== 0 || numeroCartas < 4 || numeroCartas > 14 || isNaN(numeroCartas)) {
+        return true;
+    }
 
+    else {
+        return false;
+    }
 }
-else {
-    alert("Escolha uma quantidade valida");
 
+function quantidadeCartas() {
+    while (verificadorQtdeCartas()) {
+
+        numeroCartas = Number(prompt("Escolha a quantidade de cartas para jogar: "))
+
+    }
+    distribuirCartas()
 }
+quantidadeCartas()
 
+function distribuirCartas() {
 
+    const selecionarCartas = [];
+    const cartas = document.querySelectorAll('.card');
+    let contador = 4;
+    console.log(cartas);
+
+    while (cartas > contador) {
+        cartas.innerHTML = `${selecionarCartas[contador]}`;
+        contador++;
+    }
+}
 
