6546ee4b93d79affe7a42bcf037b0ea380858297
Guilherme Oliveira <guioliveiraramos30@gmail.com>
2023-01-13 15:49:35 -0300

    quase pronto

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-let numeroCartas = Number("");
+let cardNumber;
 
-function verificadorQtdeCartas() {
-    if (numeroCartas % 2 !== 0 || numeroCartas < 4 || numeroCartas > 14 || isNaN(numeroCartas)) {
+const deck = [];
+
+const cards = [
+    "bobrossparrot",
+    "explodyparrot",
+    "fiestaparrot",
+    "metalparrot",
+    "revertitparrot",
+    "tripletsparrot",
+    "unicornparrot"
+];
+
+let firstCard, secondCard;
+
+let plays = 0;
+
+let corrects = 0;
+
+function verifyCardQuantity() {
+    if (cardNumber % 2 !== 0 || cardNumber < 4 || cardNumber > 14 || isNaN(cardNumber)) {
         return true;
     }
 
@@ -10,26 +28,126 @@ function verificadorQtdeCartas() {
     }
 }
 
-function quantidadeCartas() {
-    while (verificadorQtdeCartas()) {
 
-        numeroCartas = Number(prompt("Escolha a quantidade de cartas para jogar: "))
+function cardQuantity() {
+
+    while (verifyCardQuantity()) {
+
+        cardNumber = Number(prompt("Escolha a quantidade de cartas para jogar: "))
 
     }
-    distribuirCartas()
+
+    distributeCards();
+
 }
-quantidadeCartas()
+cardQuantity()
 
-function distribuirCartas() {
 
-    const selecionarCartas = [];
-    const cartas = document.querySelectorAll('.card');
-    let contador = 4;
-    console.log(cartas);
+function distributeCards() {
 
-    while (cartas > contador) {
-        cartas.innerHTML = `${selecionarCartas[contador]}`;
-        contador++;
+    for (let i = 0; i < (cardNumber / 2); i++) {
+        let card = cards[i];
+        deck.push(card);
+        deck.push(card);
+    }
+
+    deck.sort(toShuffle);
+
+    showCards()
+}
+
+function toShuffle() {
+    return Math.random() - 0.5;
+}
+
+
+function showCards() {
+
+    const allCards = document.querySelector('.all-cards');
+
+    for (let i = 0; i < deck.length; i++) {
+        let cardTemplate = `
+        <li class="card" onclick=turnoverCard(this)>
+        <div class="back-face face">
+            <img class="back-card" src="./Imagens/${deck[i]}.gif" alt="">
+        </div>
+        <div class="front-face face">
+            <img class="front-card" src="./Imagens/back.png">
+        </div>
+    </li> 
+        `;
+
+        allCards.innerHTML += cardTemplate;
     }
 }
+
+function turnoverCard(card) {
+
+    if (card.classList.contains('turnover')) {
+
+        return;
+
+    }
+
+    if (firstCard !== undefined && secondCard !== undefined) {
+
+        return;
+
+    }
+
+
+    card.classList.add('turnover');
+    plays++;
+
+    if (firstCard === undefined) {
+        firstCard = card;
+    } else {
+        if (secondCard === undefined) {
+            secondCard = card;
+
+            if (firstCard.innerHTML === secondCard.innerHTML) {
+
+                resetCards();
+
+                corrects += 2;
+
+                endGame();
+            } else {
+
+                setTimeout(hideCards, 1000);
+
+            }
+        }
+    }
+}
+
+
+function hideCards() {
+
+    firstCard.classList.remove('turnover');
+
+    secondCard.classList.remove('turnover');
+
+    resetCards();
+}
+
+
+function resetCards() {
+
+    firstCard = undefined;
+    secondCard = undefined;
+
+}
+
+
+function endGame() {
+
+    if (corrects === deck.length) {
+
+        alert(`Você ganhou com ${plays} jogadas!`);
+
+    }
+}
+
+
 
